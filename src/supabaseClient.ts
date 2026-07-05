e863912eb1f640ba3dccab2e8f6bbbd781696605
elqaddaoui <elqaddaouiayoub@gmail.com>
2026-07-05 17:11:42 +0100

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://whjdybmttuyuhxshulva.supabase.co/rest/v1/'
+const SUPABASE_URL = 'https://whjdybmttuyuhxshulva.supabase.co'
 const SUPABASE_PUBLIC_KEY = 'sb_publishable_T4HWIrRkNyOz00OP6VcqjA_iBNjlbq2'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLIC_KEY)
